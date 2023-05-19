13d4d6f2d3f19fdc939f359a7ff0ff4f1c24c2bf
gyl <im.ggyyll@gmail.com>
2023-05-19 18:21:14 +0800

    update lsp config

--- lua/plugins/lsp_zero.lua
+++ lua/plugins/lsp_zero.lua
@@ -1,34 +1,77 @@
- return {
+return {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     dependencies = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' }, -- Required
+        {
+            -- Optional
+            'williamboman/mason.nvim',
+            build = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },     -- Required
+        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+        { 'L3MON4D3/LuaSnip' },     -- Required
     },
     config = function()
-        local lsp = require('lsp-zero').preset({})
+        local lsp = require('lsp-zero').preset({
+            manage_nvim_cmp = {
+                set_sources = 'lsp',
+                set_basic_mappings = true,
+                set_extra_mappings = false,
+                use_luasnip = true,
+                set_format = true,
+                documentation_window = true,
+            }
+        })
         lsp.on_attach(function(client, bufnr)
-          lsp.default_keymaps({buffer = bufnr})
+            lsp.default_keymaps({ buffer = bufnr })
+            lsp.buffer_autoformat()
         end)
         require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+        lsp.set_sign_icons({
+            error = '✘',
+            warn = '▲',
+            hint = '⚑',
+            info = '»'
+        })
+        lsp.omnifunc.setup({
+            autocomplete = true,
+            use_fallback = true,
+            update_on_delete = true,
+        })
+        require('lsp-zero').extend_lspconfig()
         lsp.setup()
         local cmp = require('cmp')
+        local cmp_action = require('lsp-zero').cmp_action()
         cmp.setup({
-          mapping = {
-            ['<CR>'] = cmp.mapping.confirm({select = true}),
-          }
+            preselect = 'item',
+            completion = {
+                completeopt = 'menu,menuone,noinsert'
+            },
+            formatting = {
+                fields = { 'menu', 'abbr', 'kind' },
+                format = function(entry, item)
+                    local menu_icon = {
+                        nvim_lsp = 'λ',
+                        luasnip = '⋗',
+                        buffer = 'Ω',
+                        path = '🖫',
+                        nvim_lua = 'Π',
+                    }
+                    item.menu = menu_icon[entry.source.name]
+                    return item
+                end,
+            },
+            mapping = {
+                ['<Tab>'] = cmp_action.tab_complete(),
+                ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+            }
         })
     end
-  }
+}
