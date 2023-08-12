ee8fd9fdad902de70a3eae33b8212d424a8dfd3d
gyl30 <im.ggyyll@gmail.com>
2023-08-12 17:10:41 +0800

    disable lspsage

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,6 +1,7 @@
 return {
         "glepnir/lspsaga.nvim",
         event = "LspAttach",
+        enabled = false,
         config = function() require("lspsaga").setup {} end,
         dependencies = {
             { "nvim-tree/nvim-web-devicons" },
