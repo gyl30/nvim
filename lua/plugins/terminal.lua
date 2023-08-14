c7eb8f9d009d126df3b888af524a7e3c471c4f5b
gyl30 <im.ggyyll@gmail.com>
2023-08-14 16:46:42 +0800

    update terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,3 +1,29 @@
 return {
-    "skywind3000/vim-terminal-help", event = 'VeryLazy'
+    "akinsho/toggleterm.nvim",
+    cmd = { "ToggleTerm", "TermExec" },
+    config = function()
+        require("toggleterm").setup({
+            size = 10,
+            on_create = function()
+                vim.opt.foldcolumn = "0"
+                vim.opt.signcolumn = "no"
+            end,
+            shading_factor = 2,
+            direction = "float",
+            float_opts = {
+                border = "curved",
+                highlights = { border = "Normal", background = "Normal" },
+            },
+        })
+        local opts = { noremap = true, silent = true }
+    end,
+    keys = {
+        {
+            '<leader>ft',
+            mode = { 'n' },
+            silent = true,
+            '<cmd>ToggleTerm direction=float<cr>',
+            desc = 'Floating Terminal'
+        },
+    },
 }
