9dbc57d2ef0787a3cb5596e0bb7b1e09e5e60797
gyl30 <im.ggyyll@gmail.com>
2023-08-16 10:47:06 +0800

    update bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,30 +1,62 @@
-local bufferline_init = function()
-  for i = 1, 9 do
-    vim.keymap.set("n", "<localleader>" .. i, function() require("bufferline").go_to_buffer(i, true) end)
-  end
-end
+local config = function()
+    local get_hex = require('cokeline.utils').get_hex
+    require('cokeline').setup({
+        default_hl = {
+            fg = function(buffer)
+                return
+                    buffer.is_focused
+                    and get_hex('Normal', 'fg')
+                    or get_hex('Comment', 'fg')
+            end,
+            bg = 'NONE',
+        },
 
-local bufferline_cfg = function()
-  require("bufferline").setup {
-    options = {
-      mode = "buffers",
-      numbers = "ordinal",
-      indicator = { icon = "" },
-      max_name_length = 20,
-      max_prefix_length = 2,
-      modified_icon = "●",
-      persist_buffer_sort = false,
-      show_buffer_close_icons = false,
-      show_buffer_icons = false,
-      show_close_icon = false,
-      name_formatter = function(opts) return string.format("%s", opts.name) end,
-    },
-  }
-end
+        components = {
+            {
+                text = function(buffer) return (buffer.index ~= 1) and '▏' or '' end,
+                fg = get_hex('Normal', 'fg')
+            },
+            {
+                text = function(buffer) return buffer.index .. ' ' end,
+            },
+            {
+                text = function(buffer) return buffer.unique_prefix end,
+                fg = get_hex('Comment', 'fg'),
+                italic = true,
+            },
+            {
+                text = function(buffer) return buffer.devicon.icon end,
+                fg = function(buffer) return buffer.devicon.color end,
+            },
 
+            {
+                text = function(buffer) return buffer.filename .. '  ' end,
+                style = function(buffer) return buffer.is_focused and 'bold' or nil end,
+            },
+            {
+                text = '',
+                on_click = function(buffer)
+                    buffer:delete()
+                end,
+                on_mouse_enter = function() vim.opt.mouse = 'a' end,
+                on_mouse_enter = function() vim.opt.mouse = '' end,
+            },
+            {
+                text = '  ',
+            },
+        },
+    })
+    local opt = { silent = true, expr = true }
+    vim.keymap.set('n', '<tab>',
+        function() return ('<Plug>(cokeline-focus-%s)'):format(vim.v.count > 0 and vim.v.count or 'next') end, opt)
+    for i = 1, 9 do
+        vim.api.nvim_set_keymap("n", ("<localleader>%s"):format(i), ("<Plug>(cokeline-focus-%s)"):format(i), opt)
+    end
+end
 return {
-    "akinsho/nvim-bufferline.lua",
-    event = { "BufReadPre", "BufNewFile" },
-    init = bufferline_init,
-    config = bufferline_cfg,
+    "willothy/nvim-cokeline",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = config,
 }
