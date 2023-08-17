fb891ec8272fa241721d52664fe3d832c0165aa7
gyl30 <im.ggyyll@gmail.com>
2023-08-17 22:37:58 +0800

    autoformat autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -102,18 +102,38 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     end,
 })
 
-vim.api.nvim_create_autocmd("CursorHold", {
+-- vim.api.nvim_create_autocmd("CursorHold", {
+--     callback = function()
+--         vim.signcolumn = "no"
+--         vim.diagnostic.open_float(nil, { show_header = false, severity_sort = true, scope = "line", focusable = false })
+--     end,
+-- })
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = '*',
     callback = function()
-        vim.signcolumn = "no"
-        vim.diagnostic.open_float(nil, { show_header = false, severity_sort = true, scope = "line", focusable = false })
+        local clients = vim.lsp.buf_get_clients()
+        if #clients > 0 then
+            vim.lsp.buf.format({ timeout_ms = 3000, async = false, })
+        end
     end,
 })
 
-vim.api.nvim_create_autocmd('BufWritePre', {
-    pattern = '*.go',
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = { "*.go" },
     callback = function()
-        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
-    end
+        local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+        for _, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+                else
+                    vim.lsp.buf.execute_command(r.command)
+                end
+            end
+        end
+    end,
 })
 
 local lsp_cli_name = function()
@@ -143,6 +163,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "S", vim.lsp.buf.signature_help, opts)
         vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
-        vim.keymap.set('n', '<space>d', builtin.diagnostics, opts)
+        vim.keymap.set('n', '<space>d', "<cmd>Lspsaga show_buf_diagnostics<cr>", opts)
+        vim.keymap.set('n', '<space>m', "cmd>Telescope man_pages sections={\"ALL\"}<cr>", opts)
     end,
 })
