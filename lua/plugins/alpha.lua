94c9807320897871af18a14a0aa9b8ad97a6b7d5
gyl30 <im.ggyyll@gmail.com>
2023-09-14 20:59:47 +0800

    update alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,10 +1,5 @@
 return {
-        "goolord/alpha-nvim",
-        lazy = true,
-        event = "VimEnter",
-        config = function()
-            local alpha = require("alpha")
-            local startify = require("alpha.themes.startify")
-            alpha.setup(startify.config)
-        end,
-    }
+    "mhinz/vim-startify",
+    lazy = true,
+    event = "VimEnter",
+}
