44a4802f8c23f1f50677481e3179cd6819316df1
gyl30 <im.ggyyll@gmail.com>
2023-09-22 16:19:40 +0800

    use startify_custom_header

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,4 +2,9 @@ return {
     "mhinz/vim-startify",
     lazy = true,
     event = "VimEnter",
+    config = function()
+        vim.cmd[[
+            let g:startify_custom_header ='startify#center(startify#fortune#cowsay())'
+        ]]
+    end
 }
