ed84ad6f6307f9348957abc0182848d71b80703d
gyl30 <im.ggyyll@gmail.com>
2023-10-26 22:39:00 +0800

    add translate

---
+++ lua/plugins/translate.lua
@@ -0,0 +1,12 @@
+return {
+    "gyl30/translate",
+    config = function()
+        vim.cmd [[
+            vmap <silent> <leader>t <Cmd>lua require("translate").translateV()<cr>
+            nmap <silent> <leader>t <Cmd>lua require("translate").translateN()<cr>
+
+            command! Translate  lua require("translate").translateN()
+            command! TranslateV lua require("translate").translateV()
+        ]]
+    end
+}
