aacbc8a804022d185c3281f3ade60d24ecf03312
gyl30 <im.ggyyll@gmail.com>
2023-11-03 10:46:03 +0800

    add gitblame keymap and config to lualine

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,4 @@ vim.cmd [[
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set('n', '<localleader>r', "<cmd>Telescope asynctasks all<cr>", opts)
+vim.keymap.set('n', '<C-g>', "<cmd>GitBlameToggle<cr>", opts)
