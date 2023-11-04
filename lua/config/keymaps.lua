4d8f90786a0cb7cde6a644326bfd95b000336a58
gyl30 <im.ggyyll@gmail.com>
2023-11-04 18:07:37 +0800

    keymap lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,9 @@
-vim.cmd [[
-    nnoremap <space><space> %
-    nnoremap <C-h> <C-w>h
-    nnoremap <C-l> <C-w>l
-    nnoremap <C-j> <C-w>j
-    nnoremap <C-k> <C-w>k
-    nnoremap <leader>q :q!<CR>
-]]
-
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<leader>q", ":q!<cr>")
+vim.keymap.set("n", "<space><space>", "%")
 
 local opts = { noremap = true, silent = true }
 
