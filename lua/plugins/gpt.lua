e3c459055eb4f6eac6d9f8f5b64b6775874348ad
gyl30 <im.ggyyll@gmail.com>
2024-05-20 10:32:55 +0800

    git commit llama3

--- lua/plugins/gpt.lua
+++ lua/plugins/gpt.lua
@@ -8,6 +8,6 @@ return {
         -- vim.g.gpt_commit_proxy = 'https://127.0.0.1:8889'
         vim.g.gpt_commit_engine = 'ollama'
         vim.g.gpt_commit_ollama_url = 'http://127.0.0.1:11434/api/chat'
-        vim.g.gpt_commit_ollama_model = 'llama2'
+        vim.g.gpt_commit_ollama_model = 'llama3'
     end,
 }
