b5f2210a8daf82f9cf9b531fca18bd59e0045a15
gyl30 <im.ggyyll@gmail.com>
2024-05-20 10:33:35 +0800

    Add key binding

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,19 +3,36 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("fzf-lua").setup({
-            -- winopts = {
-            --     preview = {
-            --         -- layout = "horizontal"
-            --         layout = "vertical"
-            --     }
-            -- }
+            winopts = {
+                preview = {
+                    layout = "vertical"
+                }
+            }
         })
-
+        vim.api.nvim_create_autocmd("VimResized", {
+            pattern = '*',
+            command = 'lua require("fzf-lua").redraw()'
+        })
         vim.keymap.set({ "n" }, "<C-x><C-f>",
             function()
                 require("fzf-lua").lsp_finder({
-                    winopts = { preview = { layout = "vertical" } }
+                    winopts = {
+                        preview = {
+                            layout = "vertical",
+                            vertical = "up"
+                        }
+                    }
                 })
             end, { silent = true, desc = "Fuzzy lsp finder" })
+        vim.keymap.set({ "n" }, "<C-x><C-g>",
+            function()
+                require("fzf-lua").git_files({
+                    winopts = {
+                        preview = {
+                            layout = "horizontal"
+                        }
+                    }
+                })
+            end, { silent = true, desc = "Fuzzy git files" })
     end
 }
