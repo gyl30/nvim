c01714c3aa58e904d5bb3631affe0fe292749a1c
gyl30 <im.ggyyll@gmail.com>
2024-07-14 16:52:02 +0800

    Update fzf profile

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,37 +2,23 @@ return {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        local fzf_lua = require("fzf-lua")
         require("fzf-lua").setup({
-            winopts = {
-                preview = {
-                    layout = "vertical"
+            "borderless_full",
+            actions = {
+                files = {
+                    ["ctrl-t"]  = fzf_lua.actions.file_tabedit,
                 }
-            }
+            },
+            previewers = {
+                man = {
+                    cmd = "man %s | col -bx",
+                }
+            },
         })
         vim.api.nvim_create_autocmd("VimResized", {
             pattern = '*',
             command = 'lua require("fzf-lua").redraw()'
         })
-        vim.keymap.set({ "n" }, "<C-x><C-f>",
-            function()
-                require("fzf-lua").lsp_finder({
-                    winopts = {
-                        preview = {
-                            layout = "vertical",
-                            vertical = "up"
-                        }
-                    }
-                })
-            end, { silent = true, desc = "Fuzzy lsp finder" })
-        vim.keymap.set({ "n" }, "<C-x><C-g>",
-            function()
-                require("fzf-lua").git_files({
-                    winopts = {
-                        preview = {
-                            layout = "horizontal"
-                        }
-                    }
-                })
-            end, { silent = true, desc = "Fuzzy git files" })
     end
 }
