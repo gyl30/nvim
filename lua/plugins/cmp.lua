c62e1a17b554dc2aa99886a19c724e9324a5766d
gyl30 <im.ggyyll@gmail.com>
2024-09-26 13:42:52 +0800

    精简 cmp 配置

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,66 +1,43 @@
-local kind_icons = {
-    Text = "",
-    Method = "",
-    Function = "",
-    Constructor = "",
-    Field = "ﰠ",
-    Variable = "",
-    Class = "ﴯ",
-    Interface = "",
-    Module = "",
-    Property = "ﰠ",
-    Unit = "塞",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "פּ",
-    Event = "",
-    Operator = "",
-    TypeParameter = "",
-    Supermaven = "",
-    Codeium = "",
-}
+local function formatter(entry, vim_item)
+    local icon, hl, is_default = require("mini.icons").get("lsp", vim_item.kind)
+    if is_default then
+        icon = "󰞋"
+        hl = "CmpItemKind" .. vim_item.kind
+    end
+    vim_item.kind = icon
+    vim_item.kind_hl_group = hl
+    vim_item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[Lsp]",
+        nvim_lua = "[Lua]",
+        path = "[Path]",
+        codeium = "[Codeium]",
+        supermaven = "[SuperMaven]",
+    })[entry.source.name]
+    return vim_item
+end
+
+local function has_words_before()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and
+        vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 return {
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
-            "saadparwaiz1/cmp_luasnip",
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-nvim-lua",
             "hrsh7th/cmp-nvim-lsp",
-            "onsails/lspkind.nvim",
+            { 'echasnovski/mini.nvim', version = false },
         },
         event = "InsertEnter",
         opts = function()
             local cmp = require "cmp"
-            local snip_status_ok, luasnip = pcall(require, "luasnip")
-            local lspkind_status_ok, lspkind = pcall(require, "lspkind")
-            if not snip_status_ok then return end
-            local border_opts = {
-                border = "single",
-                winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-            }
-
-            local function has_words_before()
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and
-                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-            end
-
             return {
                 preselect = cmp.PreselectMode.None,
-                snippet = {
-                    expand = function(args) luasnip.lsp_expand(args.body) end,
-                },
-
                 completion = {
                     keyword_length = 2,
                 },
@@ -71,65 +48,17 @@ return {
                     disallow_partial_matching = false,
                     disallow_prefix_unmatching = true,
                 },
-                sorting = {
-                    priority_weight = 2,
-                    comparators = {
-                        cmp.config.compare.offset,
-                        cmp.config.compare.exact,
-                        cmp.config.compare.score,
-                        cmp.config.compare.recently_used,
-                        cmp.config.compare.kind,
-                        cmp.config.compare.sort_text,
-                        cmp.config.compare.length,
-                        cmp.config.compare.order,
-                    }
-                },
-                duplicates = {
-                    nvim_lsp = 1,
-                    luasnip = 1,
-                    cmp_tabnine = 1,
-                    buffer = 1,
-                    path = 1,
-                },
-                confirm_opts = {
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = false,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(border_opts),
-                    documentation = cmp.config.window.bordered(border_opts),
-                },
                 mapping = {
-                    ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-                    ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-                    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-                    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-                    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-                    ["<C-y>"] = cmp.config.disable,
-                    ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-                    ["<CR>"] = cmp.mapping.confirm { select = false },
                     ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        elseif has_words_before() then
-                            cmp.complete()
+                        if cmp.visible() and has_words_before() then
+                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
                         else
                             fallback()
                         end
                     end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    ["<S-Tab>"] = cmp.mapping(function()
                         if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
+                            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
                         end
                     end, { "i", "s" }),
                 },
@@ -138,31 +67,14 @@ return {
                     { name = "supermaven", priority = 920 },
                     { name = "codeium",    priority = 900 },
                     { name = "nvim_lua",   priority = 800 },
-                    { name = "luasnip",    priority = 750 },
                     { name = "buffer",     priority = 500 },
                     { name = "path",       priority = 250 },
                 },
                 formatting = {
-                    -- fields = { "abbr", "menu", "kind" },
                     mode = "symbol",
-                    maxwidth = 50,
+                    maxwidth = 80,
                     with_text = true,
-                    format = function(entry, vim_item)
-                        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-                        vim_item.menu = ({
-                            buffer = "[Buffer]",
-                            nvim_lsp = "[Lsp]",
-                            nvim_lua = "[Lua]",
-                            path = "[Path]",
-                            codeium = "[Codeium]",
-                            supermaven = "[SuperMaven]",
-                        })[entry.source.name]
-                        return vim_item
-                    end
-                },
-                experimental = {
-                    native_menu = false,
-                    ghost_text = false,
+                    format = formatter,
                 },
             }
         end,
