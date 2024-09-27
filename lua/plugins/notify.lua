f49583e749b445787e44b0dbf8619ec660d4c13c
gyl30 <im.ggyyll@gmail.com>
2024-09-27 09:30:51 +0800

    fix notify bg warn

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,7 +1,9 @@
 return {
-  "rcarriga/nvim-notify", 
-  event = "VimEnter",
-  config = function()
-      vim.notify = require("notify")
-  end
+    "rcarriga/nvim-notify",
+    event = "VimEnter",
+    config = function()
+        require('notify').setup({
+            background_colour = "#000000"
+        })
+    end
 }
