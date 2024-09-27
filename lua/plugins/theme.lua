78dc23739e2217c185a6cda74f44bbd40184dcc7
gyl30 <im.ggyyll@gmail.com>
2024-09-27 09:32:18 +0800

    update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,44 +1,12 @@
 return {
-    {
-        'Mofiqul/dracula.nvim',
-        config = function()
-            require("dracula").setup({
-                overrides = function(colors)
-                    return {
-                        Normal = { bg = "NONE" }, -- set NonText fg to white of theme
-                    }
-                end,
-            })
-        end
-
-    },
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
+    'Mofiqul/dracula.nvim',
     config = function()
-        require("tokyonight").setup({
-            on_highlights = function(hl, c)
-                hl.Normal = { bg = "NONE" }
-                hl.TelescopePromptNormal = {
-                    bg = '#2d3149',
+        require("dracula").setup({
+            overrides = function(colors)
+                return {
+                    Normal = { bg = "NONE" },
                 }
-                hl.TelescopePromptBorder = {
-                    bg = '#2d3149',
-                }
-                hl.TelescopePromptTitle = {
-                    fg = '#2d3149',
-                    bg = '#2d3149',
-                }
-                hl.TelescopePreviewTitle = {
-                    fg = '#1F2335',
-                    bg = '#1F2335',
-                }
-                hl.TelescopeResultsTitle = {
-                    fg = '#1F2335',
-                    bg = '#1F2335',
-                }
-            end
+            end,
         })
     end
-
 }
