762b67b7afa498a776b81cf7b7abfc523ed9dd61
gyl30 <im.ggyyll@gmail.com>
2024-09-28 21:38:19 +0800

    update hi

--- init.lua
+++ init.lua
@@ -6,5 +6,11 @@ require("config.options")
 require("config.autocmds")
 require("config.statusline")
 vim.cmd [[
-source ~/.config/nvim/task.vim
+    source ~/.config/nvim/task.vim
+    colorscheme slate
+    hi Normal guibg=None
+    highlight MatchParen guibg=NONE guifg=yellow gui=underline
+    highlight NormalFloat guibg=NONE
+    highlight FloatBorder guibg=NONE
 ]]
+
