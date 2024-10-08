c822136a69f25aebfbc656c41caa18d1d5db3381
gyl30 <im.ggyyll@gmail.com>
2024-10-08 16:49:58 +0800

    remove codeium.vim

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -3,24 +3,12 @@ return {
         "Exafunction/codeium.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
+             "iguanacucumber/magazine.nvim",
         },
         event        = "VeryLazy",
         config       = function()
             require("codeium").setup({})
         end,
         ft           = { "cpp", "hpp", "h", "go", "python" }
-    },
-    {
-        "Exafunction/codeium.vim",
-        event        = "VeryLazy",
-        config = function()
-            vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-            vim.keymap.set("i", "<C-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-            vim.keymap.set("i", "<C-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-            vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
-            vim.keymap.set("i", "<C-/>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
-        end,
-        ft     = { "cpp", "hpp", "h", "go", "python" }
     },
 }
