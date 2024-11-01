02a387afc4f2aa7d358107ae3f19d7f7d9b6ab2f
gyl30 <im.ggyyll@gmail.com>
2024-11-01 18:04:04 +0800

    Add ccls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,30 +10,30 @@ vim.lsp.handlers['workspace/diagnostic/refresh'] = function(_, _, ctx)
 end
 
 vim.diagnostic.config {
-  virtual_text = false
+    virtual_text = false
 }
 
 local ns = vim.api.nvim_create_namespace('CurlineDiag')
-vim.api.nvim_create_autocmd('LspAttach',{
-  callback = function(args)
-    vim.api.nvim_create_autocmd('CursorHold', {
-      buffer = args.buf,
-      callback = function()
-        pcall(vim.api.nvim_buf_clear_namespace,args.buf,ns, 0, -1)
-        local hi = { 'Error', 'Warn','Info','Hint'}
-        local curline = vim.api.nvim_win_get_cursor(0)[1]
-        local diagnostics = vim.diagnostic.get(args.buf, {lnum =curline - 1})
-        local virt_texts = { { (' '):rep(4) } }
-        for _, diag in ipairs(diagnostics) do
-          virt_texts[#virt_texts + 1] = {diag.message, 'Diagnostic'..hi[diag.severity]}
-        end
-        vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0,{
-          virt_text = virt_texts,
-          hl_mode = 'combine'
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        vim.api.nvim_create_autocmd('CursorHold', {
+            buffer = args.buf,
+            callback = function()
+                pcall(vim.api.nvim_buf_clear_namespace, args.buf, ns, 0, -1)
+                local hi = { 'Error', 'Warn', 'Info', 'Hint' }
+                local curline = vim.api.nvim_win_get_cursor(0)[1]
+                local diagnostics = vim.diagnostic.get(args.buf, { lnum = curline - 1 })
+                local virt_texts = { { (' '):rep(4) } }
+                for _, diag in ipairs(diagnostics) do
+                    virt_texts[#virt_texts + 1] = { diag.message, 'Diagnostic' .. hi[diag.severity] }
+                end
+                vim.api.nvim_buf_set_extmark(args.buf, ns, curline - 1, 0, {
+                    virt_text = virt_texts,
+                    hl_mode = 'combine'
+                })
+            end
         })
-      end
-    })
-  end
+    end
 })
 local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -41,7 +41,26 @@ local on_attach = function(client, bufnr)
     vim.cmd([[highlight DiagnosticFloatingWarn guibg=NONE guifg=NONE gui=underline]])
     vim.cmd([[highlight DiagnosticFloatingInfo guibg=NONE guifg=NONE gui=underline]])
     vim.cmd([[highlight DiagnosticFloatingHint guibg=NONE guifg=NONE gui=underline]])
+    if client.supports_method 'textDocument/documentHighlight' then
+        vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true })
+        vim.api.nvim_clear_autocmds { buffer = bufnr, group = 'lsp_document_highlight' }
+        vim.api.nvim_create_autocmd('CursorHold', {
+            callback = vim.lsp.buf.document_highlight,
+            buffer = bufnr,
+            group = 'lsp_document_highlight',
+            desc = 'Document Highlight',
+        })
+        vim.api.nvim_create_autocmd('CursorMoved', {
+            callback = vim.lsp.buf.clear_references,
+            buffer = bufnr,
+            group = 'lsp_document_highlight',
+            desc = 'Clear All the References',
+        })
+    end
 
+    if client.server_capabilities.semanticTokensProvider then
+        vim.treesitter.stop(bufnr)
+    end
     if client.server_capabilities.documentHighlightProvider then
         vim.api.nvim_create_autocmd("CursorMoved", {
             callback = vim.lsp.buf.clear_references,
@@ -124,6 +143,17 @@ local gopls_options = {
         },
     },
 }
+local ccls_options = {
+    init_options = {
+        index = {
+            threads = 0,
+            initialBlacklist = { "/(test|unittests)/" },
+        },
+        highlight = {
+            rainbow = 10,
+        },
+    }
+}
 local clangd_options = {
     on_attach = on_attach,
     settings = {
@@ -183,19 +213,63 @@ local config = function()
     }
     gopls_options.capabilities = lsp_capabilities
     clangd_options.capabilities = lsp_capabilities
+    clangd_options.capabilities.offsetEncoding = { "utf-32" }
+    ccls_options.capabilities = lsp_capabilities
     lua_ls_options.capabilities = lsp_capabilities
     lspconfig.gopls.setup(gopls_options)
     lspconfig.clangd.setup(clangd_options)
+    lspconfig.ccls.setup(ccls_options)
     lspconfig.lua_ls.setup(lua_ls_options)
 end
+local func_colors = {
+    '#e5b124', '#927754', '#eb992c', '#e2bf8f', '#d67c17',
+    '#88651e', '#e4b953', '#a36526', '#b28927', '#d69855',
+}
+local type_colors = {
+    '#e1afc3', '#d533bb', '#9b677f', '#e350b6', '#a04360',
+    '#dd82bc', '#de3864', '#ad3f87', '#dd7a90', '#e0438a',
+}
+local param_colors = {
+    '#e5b124', '#927754', '#eb992c', '#e2bf8f', '#d67c17',
+    '#88651e', '#e4b953', '#a36526', '#b28927', '#d69855',
+}
+local var_colors = {
+    '#429921', '#58c1a4', '#5ec648', '#36815b', '#83c65d',
+    '#419b2f', '#43cc71', '#7eb769', '#58bf89', '#3e9f4a',
+}
+local all_colors = {
+    class = type_colors,
+    constructor = func_colors,
+    enum = type_colors,
+    enumMember = var_colors,
+    field = var_colors,
+    ['function'] = func_colors,
+    method = func_colors,
+    parameter = param_colors,
+    struct = type_colors,
+    typeAlias = type_colors,
+    typeParameter = type_colors,
+    variable = var_colors
+}
+for type, colors in pairs(all_colors) do
+    for i = 1, #colors do
+        vim.api.nvim_set_hl(0, string.format('@lsp.typemod.%s.id%s.cpp', type, i - 1), { fg = colors[i] })
+    end
+end
 
+vim.cmd([[
+hi @lsp.mod.classScope.cpp gui=italic
+hi @lsp.mod.static.cpp gui=bold
+hi @lsp.typemod.variable.namespaceScope.cpp gui=bold,underline
+]])
 return {
     'neovim/nvim-lspconfig',
     config = config,
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        { 'iguanacucumber/magazine.nvim'},
+        { 'iguanacucumber/magazine.nvim' },
         { 'hrsh7th/cmp-nvim-lsp' },
+        { 'ranjithshegde/ccls.nvim' },
         { 'p00f/clangd_extensions.nvim' },
         {
             'ray-x/lsp_signature.nvim',
