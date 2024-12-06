dfdf1a3ea9f97d41beda2978c8be7b4ee06c5976
gyl30 <im.ggyyll@gmail.com>
2024-12-06 21:04:14 +0800

    Update theme

--- init.lua
+++ init.lua
@@ -6,15 +6,7 @@ require("config.options")
 require("config.autocmds")
 require("config.statusline")
 require("config.neovide")
+require("config.theme")
 vim.cmd [[
-    colorscheme slate
     source ~/.config/nvim/task.vim
-    highlight ModeMsg     guibg=NONE guibg=NONE ctermfg=10 guifg=NvimLightGreen
-    highlight Normal      guibg=NONE
-    highlight MatchParen  guibg=NONE guifg=yellow gui=underline
-    highlight NormalFloat guibg=NONE
-    highlight FloatBorder guibg=NONE
-    highlight TabLineFill guifg=NONE guibg=NONE ctermfg=210 ctermbg=236
 ]]
-
-
