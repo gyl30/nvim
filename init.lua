5fcb05711ea33a7828ab8ed7c5021fba478c8026
gyl30 <im.ggyyll@gmail.com>
2025-01-14 18:56:30 +0800

    添加光标闪烁

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ require("config.statusline")
 require("config.neovide")
 require("config.theme")
 vim.cmd [[
+    set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175
     colorscheme slate 
     highlight ModeMsg     guibg=NONE guibg=NONE ctermfg=10 guifg=NvimLightGreen
     highlight Normal      guibg=NONE
