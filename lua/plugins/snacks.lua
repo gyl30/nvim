df790f2b1a5e4f06a30e3ec7b4c5da7ee42f86fa
gyl30 <im.ggyyll@gmail.com>
2025-01-24 17:24:47 +0800

    Update snack key mapping

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,7 @@ return {
         bigfile = { enabled = true },
         dashboard = {
             sections = {
-                { section = "terminal",     cmd = "fortune -s | cowsay",          hl = "header", padding = 0, indent = 8,  height = 15, },
+                { section = "terminal",     cmd = "fortune -s | cowsay",          hl = "header", padding = 0, indent = 8, height = 15, },
                 { title = "MRU",            padding = 0 },
                 { section = "recent_files", limit = 8,                            padding = 1 },
                 { title = "MRU ",           file = vim.fn.fnamemodify(".", ":~"), padding = 0 },
@@ -14,16 +14,13 @@ return {
                 { section = "startup" },
             },
         },
-        -- indent = { enabled = true },
         lazygit = { enabled = true },
     },
     keys = {
-        { "<leader>z",  function() Snacks.zen() end,                   desc = "Toggle Zen Mode" },
-        { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
-        { "<leader>cR", function() Snacks.rename.rename_file() end,    desc = "Rename File" },
-        { "<leader>gf", function() Snacks.lazygit.log_file() end,      desc = "Lazygit Current File History" },
-        { "<leader>gg", function() Snacks.lazygit() end,               desc = "Lazygit" },
-        { "<leader>gl", function() Snacks.lazygit.log() end,           desc = "Lazygit Log (cwd)" },
-        { "<leader>un", function() Snacks.notifier.hide() end,         desc = "Dismiss All Notifications" },
+        { "<leader>z",  function() Snacks.zen() end,                desc = "Toggle Zen Mode" },
+        { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+        { "<leader>gf", function() Snacks.lazygit.log_file() end,   desc = "Lazygit Current File History" },
+        { "<leader>gg", function() Snacks.lazygit() end,            desc = "Lazygit" },
+        { "<leader>gl", function() Snacks.lazygit.log() end,        desc = "Lazygit Log (cwd)" },
     },
 }
