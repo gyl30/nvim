c77d4c071e331ad948f865531fd6338deb2a0b7f
gyl30 <im.ggyyll@gmail.com>
2025-04-01 13:46:48 +0800

    Add treewalker

---
+++ lua/plugins/treewalker.lua
@@ -0,0 +1,14 @@
+return {
+    'aaronik/treewalker.nvim',
+    opts = {
+        highlight = true,
+        highlight_duration = 250,
+        highlight_group = 'CursorLine',
+    },
+    keys = {
+        { "<C-h>", "<cmd>Treewalker Left<cr>",  desc = "Treewalker left" },
+        { "<C-l>", "<cmd>Treewalker Right<cr>", desc = "Treewalker right" },
+        { "<C-j>", "<cmd>Treewalker Down<cr>",  desc = "Treewalker down" },
+        { "<C-k>", "<cmd>Treewalker Up<cr>",    desc = "Treewalker up" },
+    },
+}
