307332f6ced6769978ebb35ca9e46571b75673b4
gyl30 <im.ggyyll@gmail.com>
2025-04-01 13:48:58 +0800

    Rename python path

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,7 +25,6 @@ vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.virtualedit = 'onemore'
 
 vim.opt.undofile = true
@@ -35,17 +34,17 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 
-local python2_dirs = { '/user/bin/python', '/usr/local/bin/python', '/home/linuxbrew/.linuxbrew/bin/python' }
-local python3_dirs = { '/user/bin/python3', '/usr/local/bin/python3', '/home/linuxbrew/.linuxbrew/bin/python3' }
+local python2_list = { '/user/bin/python', '/usr/local/bin/python', '/home/linuxbrew/.linuxbrew/bin/python' }
+local python3_list = { '/user/bin/python3', '/usr/local/bin/python3', '/home/linuxbrew/.linuxbrew/bin/python3' }
 
-for _, dir in pairs(python2_dirs) do
-    if vim.loop.fs_stat(dir) then
-        vim.g.python_host_prog = dir
+for _, py in pairs(python2_list) do
+    if vim.fn.executable(py) then
+        vim.g.python_host_prog = py
     end
 end
 
-for _, dir in pairs(python3_dirs) do
-    if vim.loop.fs_stat(dir) then
-        vim.g.python3_host_prog = dir
+for _, py in pairs(python3_list) do
+    if vim.fn.executable(py) then
+        vim.g.python3_host_prog = py
     end
 end
