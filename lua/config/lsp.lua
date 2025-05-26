1208ea83c09615b62740e1647c51bff7d9e2fb36
gyl30 <im.ggyyll@gmail.com>
2025-05-26 10:14:24 +0800

    refactor(lsp): 简化文档高亮自动命令配置

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -146,13 +146,11 @@ local on_attach = function(client, bufnr)
         vim.api.nvim_create_autocmd('CursorHold', {
             callback = vim.lsp.buf.document_highlight,
             buffer = bufnr,
-            group = 'lsp_document_highlight',
             desc = 'Document Highlight',
         })
         vim.api.nvim_create_autocmd('CursorMoved', {
             callback = vim.lsp.buf.clear_references,
             buffer = bufnr,
-            group = 'lsp_document_highlight',
             desc = 'Clear All the References',
         })
     end
