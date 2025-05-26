a71ea49e0c167eeff7a204ccf51a30a08b41346a
gyl30 <im.ggyyll@gmail.com>
2025-05-26 10:08:50 +0800

    refactor(dashboard): 优化 snacks 插件仪表盘布局和功能

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -16,10 +16,10 @@ return {
         bigfile = { enabled = true },
         dashboard = {
             sections = {
-                -- { section = "terminal",     cmd = "fortune -s | cowsay",          hl = "header", padding = 0, indent = 8, height = 15, },
-                { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-                { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-                -- { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+                { title = "Recent Files", padding = 1 },
+                { section = "recent_files", limit = 8, padding = 1 },
+                { title = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
+                { section = "recent_files", cwd = true, limit = 8, padding = 1 },
                 { section = "startup" },
             },
         },
