ff076cbf11fa5240a13ec429cd2b3818203f391b
gyl30 <im.ggyyll@gmail.com>
2025-05-26 10:11:29 +0800

    feat(lsp/clangd): 扩展 Clangd 配置并增强功能支持

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,12 +1,32 @@
 return {
-    root_markers = { 'compile_commands.json', 'build/compile_commands.json' },
-    filetypes = { 'c', 'cpp' },
+    root_markers = {
+        '.clangd',
+        '.clang-tidy',
+        '.clang-format',
+        'build/debug/compile_commands.json',
+        'build/release/compile_commands.json',
+        'build/compile_commands.json',
+        'compile_commands.json',
+        'compile_flags.txt',
+        'configure.ac',
+    },
+    filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+    single_file_support = true,
+    capabilities = {
+        textDocument = {
+            completion = {
+                editsNearCursor = true,
+            },
+        },
+        offsetEncoding = { 'utf-8', 'utf-16' },
+    },
     settings = {
         clangd = {
             init_options = {
                 usePlaceholders = true,
                 completeUnimported = true,
                 clangdFileStatus = true,
+                fallbackFlags = { "--std=c++20" },
             },
         }
     },
@@ -21,8 +41,6 @@ return {
         "--pch-storage=memory",
         "--all-scopes-completion",
         "--completion-style=detailed",
-        "--compile-commands-dir=build",
         "--clang-tidy-checks=bugprone-*, clang-analyzer-*, google-*, modernize-*, performance-*, portability-*, readability-*, -bugprone-too-small-loop-variable, -clang-analyzer-cplusplus.NewDelete, -clang-analyzer-cplusplus.NewDeleteLeaks, -modernize-use-nodiscard, -modernize-avoid-c-arrays, -readability-magic-numbers, -bugprone-branch-clone, -bugprone-signed-char-misuse, -bugprone-unhandled-self-assignment, -clang-diagnostic-implicit-int-float-conversion, -modernize-use-auto, -modernize-use-trailing-return-type, -readability-convert-member-functions-to-static, -readability-make-member-function-const, -readability-qualified-auto, -readability-redundant-access-specifiers,",
-    }
-
+    },
 }
