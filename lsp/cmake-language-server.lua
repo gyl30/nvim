336ee75070b392707537434e40f4fa9834e484f9
gyl30 <im.ggyyll@gmail.com>
2026-03-14 10:19:24 +0800

    Add cmake lsp

---
+++ lsp/cmake-language-server.lua
@@ -0,0 +1,8 @@
+return {
+    cmd = { 'cmake-language-server' },
+    filetypes = { "cmake" },
+    init_options = {
+        buildDirectory = "build"
+    },
+    root_markers = { "CMakePresets.json", "CTestConfig.cmake", ".git", "build", "cmake" }
+}
